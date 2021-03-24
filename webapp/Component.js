0f3f87bd6c8d0d5cc31bbf6ffbd24cd8344bc96e
boujaafare <20012678>
2021-03-24 18:46:54 +0000

    First Comment

---
+++ webapp/Component.js
@@ -0,0 +1,7 @@
+sap.ui.define(["sap/suite/ui/generic/template/lib/AppComponent"], function (AppComponent) {
+	return AppComponent.extend("ZU_CTRL_SUPWHT.Component", {
+		metadata: {
+			"manifest": "json"
+		}
+	});
+});
